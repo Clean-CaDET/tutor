4849f9c25ddd12cfee3cf70220a3bd3a41329596
Balša Šarenac <balsa.sarenac@gmail.com>
2021-10-28 09:37:29 +0200

    feat: introduce `CodeSnippetId` field to CFS

--- src/Tutor.Core/ContentModel/LearningObjects/Challenges/FulfillmentStrategy/ChallengeFulfillmentStrategy.cs
+++ src/Tutor.Core/ContentModel/LearningObjects/Challenges/FulfillmentStrategy/ChallengeFulfillmentStrategy.cs
@@ -6,6 +6,7 @@ namespace Tutor.Core.ContentModel.LearningObjects.Challenges.FulfillmentStrategy
     public abstract class ChallengeFulfillmentStrategy
     {
         public int Id { get; private set; }
+        public string CodeSnippedId { get; protected set; }
 
         public abstract HintDirectory EvaluateSubmission(List<CaDETClass> solutionAttempt);
         public abstract List<ChallengeHint> GetAllHints();
