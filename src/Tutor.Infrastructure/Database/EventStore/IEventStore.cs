c0313d39a7bf23340d1669e5a7cf497e8efbc73c
Milana Todorovic <milanatodorovic@uns.ac.rs>
2022-03-01 14:32:49 +0100

    feat: Adds minimal interface for an event store.

---
+++ src/Tutor.Infrastructure/Database/EventStore/IEventStore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutor.Core.BuildingBlocks.EventSourcing;
+
+namespace Tutor.Infrastructure.Database.EventStore
+{
+    public interface IEventStore
+    {
+        void Save(EventSourcedAggregateRoot aggregate);
+        IEnumerable<DomainEvent> GetEvents(TimeInterval interval);
+        IEnumerable<DomainEvent> GetEventsByAggregate(int aggregateId, TimeInterval interval);
+    }
+}
