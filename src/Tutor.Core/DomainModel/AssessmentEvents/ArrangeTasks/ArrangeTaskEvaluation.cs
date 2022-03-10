8d9528d30c19a09592393e699cc175862a73ca46
lukaDoric <luka.doric@uns.ac.rs>
2022-03-10 15:18:00 +0100

    Fix according to Sonar.

--- src/Tutor.Core/DomainModel/AssessmentEvents/ArrangeTasks/ArrangeTaskEvaluation.cs
+++ src/Tutor.Core/DomainModel/AssessmentEvents/ArrangeTasks/ArrangeTaskEvaluation.cs
@@ -5,14 +5,11 @@ namespace Tutor.Core.DomainModel.AssessmentEvents.ArrangeTasks
     public class ArrangeTaskEvaluation : Evaluation
     {
         public List<ArrangeTaskContainerEvaluation> ContainerEvaluations { get; }
-        
-        private double CorrectnessLevel { get; }
-        
+
         public ArrangeTaskEvaluation(int assessmentEventId, double correctnessLevel,
             List<ArrangeTaskContainerEvaluation> containerEvaluations) : base(assessmentEventId, correctnessLevel)
         {
             ContainerEvaluations = containerEvaluations;
-            CorrectnessLevel = correctnessLevel;
         }
     }
 }
