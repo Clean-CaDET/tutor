b3d1f30040f25d070cc58b4890778f9151b08db9
Milana Todorovic <milanatodorovic@uns.ac.rs>
2022-03-16 10:18:18 +0100

    syle: Returns files with corrected names.

---
+++ src/Tutor.Web/Controllers/Events/EventDto.cs
@@ -0,0 +1,16 @@
+﻿using Tutor.Core.BuildingBlocks.EventSourcing;
+
+namespace Tutor.Web.Controllers.Events
+{
+    public class EventDto
+    {
+        public DomainEvent DomainEvent { get; set; }
+        public string Type { get; set; }
+
+        public EventDto(DomainEvent @event)
+        {
+            DomainEvent = @event;
+            Type = @event.GetType().Name;
+        }
+    }
+}
