cf367118a73aff1206aa06fecccb1103d8a10d65
Nikola Luburic <luburic91@gmail.com>
2022-04-13 11:26:48 +0200

    refactor: Removes old learner registration functionality.

--- src/Tutor.Infrastructure/Security/Authorization/AuthService.cs
+++ src/Tutor.Infrastructure/Security/Authorization/AuthService.cs
@@ -6,13 +6,11 @@ namespace Tutor.Infrastructure.Security.Authorization
 {
     public class AuthService : IAuthService
     {
-        private readonly ILearnerService _learnerService;
         private readonly ITokenService _tokenService;
         private readonly ILearnerRepository _learnerRepository;
 
-        public AuthService(ILearnerService learnerService, ITokenService tokenService, ILearnerRepository learnerRepository)
+        public AuthService(ITokenService tokenService, ILearnerRepository learnerRepository)
         {
-            _learnerService = learnerService;
             _tokenService = tokenService;
             _learnerRepository = learnerRepository;
         }
@@ -24,16 +22,6 @@ namespace Tutor.Infrastructure.Security.Authorization
             return learner.Password.Equals(PasswordUtilities.HashPassword(password, Convert.FromBase64String(learner.Salt)))
                 ? _tokenService.GenerateAccessToken(learner.Id, "learner")
                 : Result.Fail("The username or password is incorrect!");
-        }
-
-        public Result<AuthenticationResponse> Register(Learner learner)
-        {
-            var salt = PasswordUtilities.GenerateSalt();
-            var hashedPassword = PasswordUtilities.HashPassword(learner.Password, salt);
-            learner.Salt = Convert.ToBase64String(salt);
-            learner.Password = hashedPassword;
-            var result = _learnerService.Register(learner);
-            return _tokenService.GenerateAccessToken(result.Value.Id, "learner");
         }
 
         public Result<AuthenticationResponse> RefreshToken(AuthenticationTokens authenticationTokens)
