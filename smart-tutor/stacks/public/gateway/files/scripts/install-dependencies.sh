5c7e3792d701f4f4aa84eab455c8177936ccc778
DanijelRadakovic <danijelradakovic1996@gmail.com>
2022-05-16 23:10:34 +0200

    fix: remove fixed version of gateway dependencies

--- smart-tutor/stacks/public/gateway/files/scripts/install-dependencies.sh
+++ smart-tutor/stacks/public/gateway/files/scripts/install-dependencies.sh
@@ -1,9 +1,9 @@
 FRONTEND_APP_SRC_URL=$1
 
 apk --update --no-cache add \
-  curl=7.79.1-r0 \
-  tar=1.34-r0 \
-  gettext=0.21-r0 && \
+  curl \
+  tar \
+  gettext && \
 curl -L "${FRONTEND_APP_SRC_URL}" | tar -xz && \
 mv "$(find . -maxdepth 1 -type d | tail -n 1)" app && \
 cd app && \
