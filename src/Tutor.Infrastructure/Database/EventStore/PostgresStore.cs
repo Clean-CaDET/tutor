9ec34dcb82f8d40b3edc1788cea9b74f11a23f6d
Milana Todorovic <milanatodorovic@uns.ac.rs>
2022-05-20 19:35:49 +0200

    refactor: Refactors event store queries.

--- src/Tutor.Infrastructure/Database/EventStore/PostgresStore.cs
+++ src/Tutor.Infrastructure/Database/EventStore/PostgresStore.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Tutor.Core.BuildingBlocks;
 using Tutor.Core.BuildingBlocks.EventSourcing;
 using Tutor.Core.LearnerModel.DomainOverlay.KnowledgeComponentMasteries.Events;
-using Tutor.Infrastructure.Serialization;
 
 namespace Tutor.Infrastructure.Database.EventStore
 {
@@ -32,20 +32,22 @@ namespace Tutor.Infrastructure.Database.EventStore
         public List<KnowledgeComponentEvent> GetKcEvents(List<int> kcIds, List<int> learnerIds)
         {
             if (learnerIds == null) return GetAllKcEvents(kcIds);
-            // Should be refactored to send a better query to the DB. Also a better name for these methods.
-            var allEvents = _eventContext.Events
-                .Select(e => _eventSerializer.Deserialize(e.DomainEvent) as KnowledgeComponentEvent).ToList();
 
-            return allEvents.Where(e => learnerIds.Contains(e.LearnerId)
-                            && kcIds.Contains(e.KnowledgeComponentId)).ToList();
+            var events = _eventContext.Events.
+                Where(e => EF.Functions.JsonExistAll(e.DomainEvent, "KnowledgeComponentId", "LearnerId") 
+                            && kcIds.Contains(e.DomainEvent.RootElement.GetProperty("KnowledgeComponentId").GetInt32())
+                            && learnerIds.Contains(e.DomainEvent.RootElement.GetProperty("LearnerId").GetInt32()));
+
+            return events.Select(e => _eventSerializer.Deserialize(e.DomainEvent) as KnowledgeComponentEvent).ToList();
         }
 
         private List<KnowledgeComponentEvent> GetAllKcEvents(List<int> kcIds)
         {
-            var allEvents = _eventContext.Events
-                .Select(e => _eventSerializer.Deserialize(e.DomainEvent) as KnowledgeComponentEvent).ToList();
+            var events = _eventContext.Events.
+                Where(e => EF.Functions.JsonExistAll(e.DomainEvent, "KnowledgeComponentId")
+                            && kcIds.Contains(e.DomainEvent.RootElement.GetProperty("KnowledgeComponentId").GetInt32()));
 
-            return allEvents.Where(e => kcIds.Contains(e.KnowledgeComponentId)).ToList();
+            return events.Select(e => _eventSerializer.Deserialize(e.DomainEvent) as KnowledgeComponentEvent).ToList();
         }
 
         public void Save(EventSourcedAggregateRoot aggregate)
