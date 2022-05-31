ccd1c8d8f1b838da05333980e7f050bf2e03ac0e
DanijelRadakovic <danijelradakovic1996@gmail.com>
2022-05-31 09:36:23 +0200

    fix: remove keycloack configuration from frontend build

--- smart-tutor/stacks/public/gateway/files/scripts/build-app.sh
+++ smart-tutor/stacks/public/gateway/files/scripts/build-app.sh
@@ -1,15 +1,7 @@
 SMART_TUTOR_API_URL=$1
-KEYCLOAK_ON=$2
-KEYCLOAK_AUTH_URL=$3
-KEYCLOAK_REALM=$4
-KEYCLOAK_AUDIENCE=$5
 
 cd app || exit
 export API_HOST=${SMART_TUTOR_API_URL}
-export KEYCLOAK_ON=${KEYCLOAK_ON}
-export KEYCLOAK_AUTH=${KEYCLOAK_AUTH_URL}
-export REALM=${KEYCLOAK_REALM}
-export AUDIENCE=${KEYCLOAK_AUDIENCE}
 envsubst < environment.ts.template > ./src/environments/environment.ts || exit
 npm run build --prod && \
 cd dist && \
