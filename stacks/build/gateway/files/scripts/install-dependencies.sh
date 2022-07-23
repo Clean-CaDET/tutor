44cd9e680c63f47d6966add4711c4c2d15595521
DanijelRadakovic <danijelradakovic1996@gmail.com>
2022-07-23 11:01:43 +0200

    fix(Dockerfile): linter errors

--- stacks/build/gateway/files/scripts/install-dependencies.sh
+++ stacks/build/gateway/files/scripts/install-dependencies.sh
@@ -1,3 +1,3 @@
 FRONTEND_APP_SRC_URL=$1
-apk --update --no-cache add curl tar gettext;
+apk --update --no-cache add curl tar gettext
 curl -L "${FRONTEND_APP_SRC_URL}" | tar -xz && mv "$(find . -maxdepth 1 -type d | tail -n 1)" app && cd app && npm install
