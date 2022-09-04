f5740ce62e3d0b1c4f2f93a908cfa02cb3fbf66a
Milana Todorovic <milanatodorovic@uns.ac.rs>
2022-09-04 20:21:59 +0200

    chore: Adds comments explaining the AllowedTypesDiscriminatorConvention.

--- src/Tutor.Infrastructure/Database/EventStore/DefaultEventSerializer/AllowedTypesDiscriminatorConvention.cs
+++ src/Tutor.Infrastructure/Database/EventStore/DefaultEventSerializer/AllowedTypesDiscriminatorConvention.cs
@@ -9,6 +9,11 @@ using System.Text.Json.Serialization;
 
 namespace Tutor.Infrastructure.Database.EventStore.DefaultEventSerializer
 {
+    /// <summary>
+    /// Implents a Dahomey.Json discriminator policy, where the discriminator value is defined 
+    /// through a dictionary of discriminators for allowed types. 
+    /// For usage see: <see href="https://github.com/dahomey-technologies/Dahomey.Json#polymorphism"/>
+    /// </summary>
     public class AllowedTypesDiscriminatorConvention<T> : IDiscriminatorConvention where T : notnull
     {
         private readonly JsonSerializerOptions _options;
@@ -30,9 +35,24 @@ namespace Tutor.Infrastructure.Database.EventStore.DefaultEventSerializer
             _memberName = Encoding.UTF8.GetBytes(memberName);
             _jsonConverter = options.GetConverter<T>();
             foreach(var (type, discriminator) in allowedTypes)
-            {
+            {                
                 _typesByDiscriminator.Add(discriminator, type);
                 _discriminatorsByType.Add(type, discriminator);
+                /*
+                 * Adding a (type, discriminator) pair to these dictionaries allows the convention to actually read and write
+                 * discriminators for that type, which shouldn't be enabled until TryRegisterType for that type is called. However, 
+                 * in a situation where:
+                 * 1. A hierarchy contains concrete class B, and its concrete superclass class A,
+                 * 2. Both instances of class A and class B need to be (de)serialized with discriminators,
+                 * 3. The Dahomey.Json DiscriminatorConventionRegistry.RegisterType method is called for class B first, and then for class A,
+                 * The current implementation of DiscriminatorConventionRegistry will not invoke the IDiscriminatorConvention.TryRegisterType
+                 * method for class A. This means that (de)serialization of instances of class A will fail.
+                 * 
+                 * This issue was circumvented in AllowedTypesDiscriminatorConvention by moving all necessary work for setting up types and
+                 * discriminators from the TryRegisterType method to the constructor. 
+                 * Usage of AllowedTypesDiscriminatorConvention doesn't change because of this, and DiscriminatorConventionRegistry.RegisterType
+                 * should still be called for all types that need to be supported. 
+                 */
             }
         }
 
