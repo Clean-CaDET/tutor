299e44dfee68a24ac3aae11730483690b9b0fea1
Milana Todorovic <milanatodorovic@uns.ac.rs>
2022-09-27 12:02:03 +0200

    fix: Fixes mistake with incomplete interface removal in the previous commit.

--- src/Tutor.Core/LearnerModel/DomainOverlay/KnowledgeComponentMasteries/Events/AssessmentItemEvents/AssessmentItemAnswered.cs
+++ src/Tutor.Core/LearnerModel/DomainOverlay/KnowledgeComponentMasteries/Events/AssessmentItemEvents/AssessmentItemAnswered.cs
@@ -2,7 +2,7 @@
 
 namespace Tutor.Core.LearnerModel.DomainOverlay.KnowledgeComponentMasteries.Events.AssessmentItemEvents
 {
-    public class AssessmentItemAnswered : AssessmentItemEvent, ICompletionCausing, IPassCausing
+    public class AssessmentItemAnswered : AssessmentItemEvent
     {
         public Submission Submission { get; set; }
         public Evaluation Evaluation { get; set; }
