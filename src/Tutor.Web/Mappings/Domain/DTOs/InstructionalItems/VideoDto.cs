78b4be2d910f9af994551072a3b2604527786a50
Nikola Luburic <luburic91@gmail.com>
2022-10-08 11:17:57 +0200

    fix: Serving video caption to front-end.

--- src/Tutor.Web/Mappings/Domain/DTOs/InstructionalItems/VideoDto.cs
+++ src/Tutor.Web/Mappings/Domain/DTOs/InstructionalItems/VideoDto.cs
@@ -6,5 +6,6 @@ namespace Tutor.Web.Mappings.Domain.DTOs.InstructionalItems
     public class VideoDto : InstructionalItemDto
     {
         public string Url { get; set; }
+        public string Caption { get; set; }
     }
 }
