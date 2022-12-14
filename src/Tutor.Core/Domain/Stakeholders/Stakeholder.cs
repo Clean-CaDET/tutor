be3080b69466be350e1c2424ac8c8de046c4049a
Nikola Luburic <luburic91@gmail.com>
2022-12-14 08:12:20 +0100

    fix: Adds email to Stakeholder and tweaks how the instructor username is formed.

--- src/Tutor.Core/Domain/Stakeholders/Stakeholder.cs
+++ src/Tutor.Core/Domain/Stakeholders/Stakeholder.cs
@@ -7,4 +7,5 @@ public abstract class Stakeholder : Entity
     public int UserId { get; protected internal set; }
     public string Name { get; protected set; }
     public string Surname { get; protected set; }
+    public string Email { get; protected set; }
 }
