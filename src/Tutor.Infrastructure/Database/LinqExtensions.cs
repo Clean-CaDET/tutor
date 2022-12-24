d3b0cb069de01048cac036b8a5368929d3cc3be3
Eva Janković <evaj.evadesign@gmail.com>
2022-12-24 02:54:55 +0100

    fix: total count  on get paged linq extension

--- src/Tutor.Infrastructure/Database/LinqExtensions.cs
+++ src/Tutor.Infrastructure/Database/LinqExtensions.cs
@@ -12,12 +12,13 @@ public static class LinqExtensions
     /// </summary>
     public static async Task<PagedResult<T>> GetPaged<T>(this IQueryable<T> source, int pageIndex, int pageSize)
     {
+        var count = await source.CountAsync();
+
         if (pageSize != 0 && pageIndex != 0)
         {
             source = source.Skip((pageIndex - 1) * pageSize).Take(pageSize);
         }
 
-        var count = await source.CountAsync();
         var items = await source.ToListAsync();
         return new PagedResult<T>(items, count);
     }
