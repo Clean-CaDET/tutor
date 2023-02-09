9cc7bd4eb02e3db3f5f01e8c7b5f8be4880294c1
Eva Janković <evaj.evadesign@gmail.com>
2023-02-09 17:27:39 +0100

    feat: add notes export to md file

--- src/Tutor.Core/UseCases/Learning/Utilities/NoteService.cs
+++ src/Tutor.Core/UseCases/Learning/Utilities/NoteService.cs
@@ -1,5 +1,6 @@
 ﻿using FluentResults;
 using System.Collections.Generic;
+using System.Text;
 using Tutor.Core.BuildingBlocks;
 using Tutor.Core.BuildingBlocks.Generics;
 using Tutor.Core.Domain.LearningUtilities;
@@ -25,5 +26,18 @@ public class NoteService : CrudService<Note>, INoteService
         var note = _noteRepository.Get(noteId);
         if (note.LearnerId != learnerId) return Result.Fail(FailureCode.Forbidden);
         return Delete(noteId);
+    }
+
+    public Result<byte[]> GetNotesExport(int learnerId, int unitId)
+    {
+        var notes = _noteRepository.GetNotesByLearnerAndUnit(learnerId, unitId);
+        var notesSb = new StringBuilder();
+        foreach (var note in notes)
+        {
+            notesSb.Append(note.Text);
+            notesSb.Append("\n\n---\n");
+        }
+        var utf8 = new UTF8Encoding();
+        return utf8.GetBytes(notesSb.ToString());
     }
 }
