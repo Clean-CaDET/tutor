e01c42a972b81ebf98e9eda43953e4fb6af88a81
Eva Janković <evaj.evadesign@gmail.com>
2023-03-26 12:25:23 +0200

    refactor: extension method for fluent result conversion
    Method allows simplification Controller code, no if necessary. Example use is given in the top of the class.

---
+++ src/Tutor.Web/Extensions/ResultToResponseExtensions.cs
@@ -0,0 +1,25 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Tutor.Web.Extensions
+{
+    public static class ResultToResponseExtensions
+    {
+        // return result.CreateResponse(Ok, CreateErrorResponse);
+        public static ActionResult CreateResponse (
+            this Result result, 
+            Func<ActionResult> onSuccess, Func<List<IError>, ActionResult> onFailure)
+        {
+            return result.IsSuccess ? onSuccess() : onFailure(result.Errors);
+        }
+
+        public static ActionResult CreateResponse<T>(
+            this Result<T> result,
+            Func<T, ActionResult> onSuccess, Func<List<IError>, ActionResult> onFailure)
+        {
+            return result.IsSuccess ? onSuccess(result.Value) : onFailure(result.Errors);
+        }
+    }
+}
