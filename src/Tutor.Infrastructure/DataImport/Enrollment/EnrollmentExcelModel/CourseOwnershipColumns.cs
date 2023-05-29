ff22baf198bbfca6336361eff1c01584284fb870
Nikola Luburic <luburic91@gmail.com>
2023-05-29 07:11:10 +0200

    feat: Removes excel data import functionality and EPPlus dependency.

--- src/Tutor.Infrastructure/DataImport/Enrollment/EnrollmentExcelModel/CourseOwnershipColumns.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Tutor.Infrastructure.DataImport.Enrollment.EnrollmentExcelModel;
-
-internal class CourseOwnershipColumns
-{
-    public int Id { get; set; }
-    public string InstructorUsername { get; set; }
-    public string CourseCode { get; set; }
-}
