3a77721089c42a8307a2d018d47e60de3f50f279
Eva Janković <evaj.evadesign@gmail.com>
2023-07-25 17:56:18 +0200

    feat: initial api

---
+++ src/Modules/LanguageModel/Tutor.LanguageModel.API/Interfaces/IChatService.cs
@@ -0,0 +1,14 @@
+﻿using FluentResults;
+using Tutor.LanguageModel.API.Dtos;
+
+namespace Tutor.LanguageModel.API.Interfaces;
+
+public interface IChatService
+{
+    Result<ChatDto> Get(int contextId, ContextType contextType, int learnerId);
+    // ako ne postoji chat sa id-em koji je prosledjen u messageDto, kreiraj ga
+    // ako postoji, dopuni ga
+    // to je fer, nema sta da se proverava tu
+    // TODO: da li treba proveravati da li Learner sme da pristupi KC-u sa tim id-em
+    Result<ChatDto> SendMessage(MessageDto message);
+}
--- src/Modules/LanguageModel/Tutor.LanguageModel.Core/Domain/Chat.cs
+++ src/Modules/LanguageModel/Tutor.LanguageModel.Core/Domain/Chat.cs
@@ -1,8 +1,18 @@
-﻿namespace Tutor.LanguageModel.Core.Domain;
+﻿// blasphemy
+// Predlog: da se enumeracija nalazi u API projektu
+// Pros: validacija podataka, automatski se vraca 400 ako se ne prosledi validna vrednost enuma
+// Cons: import dto u modelu fuj ahahaha
+// Cons: UnitEnrollment ima obrnut pristup
+using Tutor.LanguageModel.API.Dtos;
+
+namespace Tutor.LanguageModel.Core.Domain;
 
 public class Chat
 {
     public int Id { get; private set; }
     public int LearnerId { get; private set; }
-    public string Content { get; set; }
+    // KC id
+    public int ContextId { get; private set; }
+    //public ContextType ContextType { get; private set; }
+    public string Content { get; internal set; }
 }
