cfb3a0ee2be24f01da97bf01208db8dad9415bc3
Eva Janković <evaj.evadesign@gmail.com>
2023-07-25 16:30:03 +0200

    feat: initial model

---
+++ src/Modules/LanguageModel/Tutor.LanguageModel.Core/Domain/Chat.cs
@@ -0,0 +1,8 @@
+﻿namespace Tutor.LanguageModel.Core.Domain;
+
+public class Chat
+{
+    public int Id { get; private set; }
+    public int LearnerId { get; private set; }
+    public string Content { get; set; }
+}
--- src/Modules/LanguageModel/Tutor.LanguageModel.Infrastructure/Database/LanguageModelContext.cs
+++ src/Modules/LanguageModel/Tutor.LanguageModel.Infrastructure/Database/LanguageModelContext.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Tutor.LanguageModel.Core.Domain;
 
 namespace Tutor.LanguageModel.Infrastructure.Database;
 
 public class LanguageModelContext : DbContext
 {
     public LanguageModelContext(DbContextOptions<LanguageModelContext> options) : base(options) {}
+
+    public DbSet<Token> Tokens { get; set; }
+    public DbSet<Chat> Chats { get; set; }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
