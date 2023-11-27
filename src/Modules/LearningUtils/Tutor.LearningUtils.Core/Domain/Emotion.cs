e92b5ddb34b20f9bff4ca45aa761a62e572947a6
Nikola Luburic <luburic91@gmail.com>
2023-11-27 07:57:00 +0100

    refactor: Minor cleanup.

--- src/Modules/LearningUtils/Tutor.LearningUtils.Core/Domain/Emotion.cs
+++ src/Modules/LearningUtils/Tutor.LearningUtils.Core/Domain/Emotion.cs
@@ -6,6 +6,6 @@ namespace Tutor.LearningUtils.Core.Domain;
 public class Emotion : Entity
 { 
     public int LearnerId { get; set; }
-    public DateTime TimeStamp { get; private set; } = DateTime.Now.ToUniversalTime();
-    public string EmotionValue { get; set; } //U nedostatku inspiracije za imenovanje
+    public DateTime TimeStamp { get; private set; } = DateTime.UtcNow;
+    public string? Value { get; set; }
 }
--- src/Tutor.API/Controllers/Learner/Utilities/EmotionController.cs
+++ src/Tutor.API/Controllers/Learner/Utilities/EmotionController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Tutor.BuildingBlocks.Core.UseCases;
 using Tutor.LearningUtils.Core.Domain;
 using Tutor.LearningUtils.Core.UseCases;
 using Tutor.LearningUtils.Infrastructure.Database;
@@ -13,22 +12,24 @@ namespace Tutor.API.Controllers.Learner.Utilities
     public class EmotionController : BaseApiController
 	{
         protected readonly LearningUtilsContext DbContext;
-        private readonly ILearningUtilsUnitOfWork _unitOfWOrk;
+        private readonly ILearningUtilsUnitOfWork _unitOfWork;
 
         public EmotionController(LearningUtilsContext dbContext, ILearningUtilsUnitOfWork unitOfWOrk)
         {
             DbContext = dbContext;
-            _unitOfWOrk = unitOfWOrk;
+            _unitOfWork = unitOfWOrk;
         }
 
         [HttpPost]
         public ActionResult<Emotion> SaveChat([FromBody] string emotion)
         {
-            Emotion newEmotion = new();
-            newEmotion.LearnerId = User.LearnerId();
-            newEmotion.EmotionValue = emotion;
+            Emotion newEmotion = new()
+            {
+                LearnerId = User.LearnerId(),
+                Value = emotion
+            };
             DbContext.Emotions.Attach(newEmotion);
-            var result = _unitOfWOrk.Save();
+            var result = _unitOfWork.Save();
             return CreateResponse(result);
         }
     }
